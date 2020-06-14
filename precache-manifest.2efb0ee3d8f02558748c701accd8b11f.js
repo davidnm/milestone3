2b1fc4c537ce2217421bc00411fc9582ada338de
David NoMa <davidnm@gmail.com>
2020-06-14 21:00:31 +0100

    Updates

---
+++ precache-manifest.2efb0ee3d8f02558748c701accd8b11f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c801a9460be81f82b213d05337fced1e",
+    "url": "/milestone3/index.html"
+  },
+  {
+    "revision": "a1bbc9147d2357aaaf45",
+    "url": "/milestone3/static/css/main.2f16ccb4.chunk.css"
+  },
+  {
+    "revision": "44ee5e1942edf77ddec1",
+    "url": "/milestone3/static/js/2.35701d40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/milestone3/static/js/2.35701d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1bbc9147d2357aaaf45",
+    "url": "/milestone3/static/js/main.aaa3e7c3.chunk.js"
+  },
+  {
+    "revision": "464d3de59bb2e96b9136",
+    "url": "/milestone3/static/js/runtime-main.adc87aa6.js"
+  }
+]);
